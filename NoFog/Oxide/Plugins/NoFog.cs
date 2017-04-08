b8228481c02114a8c9cd51e4febf057ff5deb503
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-04-08 17:29:47 +0200

    Initial commit

---
+++ NoFog/Oxide/Plugins/NoFog.cs
@@ -0,0 +1,17 @@
+﻿namespace Oxide.Plugins
+{
+    [Info("No Fog", "Skipcast", "1.0.0")]
+    [Description("Removes fog from the game.")]
+    public class NoFog : RustPlugin
+    {
+        private void OnServerInitialized()
+        {
+            SingletonComponent<Climate>.Instance.FogMultiplier = 0;
+        }
+
+        private void Unload()
+        {
+            SingletonComponent<Climate>.Instance.FogMultiplier = -1;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Oxide-Plugins
 A collection of my public Oxide plugins.
+
+## Setting up environment
+By default every plugin copies its output to a local rust server's oxide plugins directory. **It's dependant on a environment variable called %RustServerDev%.** It assumes the server identity is the default one (my_server_identity).
