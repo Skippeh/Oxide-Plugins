020c4affda81bbc99c7e179d2c1786a9a14e6360
Jonathan Lindahl <jonathanjl99@gmail.com>
2017-07-29 20:12:34 +0200

    [FurnaceSplitter] Added hobo barrel to whitelist and made sure that new entries in whitelist will be added to previously saved player configs.

--- FurnaceSplitter/Oxide/Plugins/FurnaceSplitter.cs
+++ FurnaceSplitter/Oxide/Plugins/FurnaceSplitter.cs
@@ -121,7 +121,8 @@ namespace Oxide.Plugins
 			"furnace",
 			"furnace.large",
 			"campfire",
-			"refinery_small_deployed"
+			"refinery_small_deployed",
+			"hobobarrel_static"
 		};
 		
 		private void OnPlayerDisconnected(BasePlayer player, string reason)
@@ -209,14 +210,25 @@ namespace Oxide.Plugins
 				allPlayerOptions[player.userID] = new PlayerOptions
 				{
 					Enabled = true,
-					TotalStacks = new Dictionary<string, int>
-					{
-						{ "furnace", 3 },
-						{ "furnace.large", 15 },
-						{ "campfire", 2 },
-						{ "refinery_small_deployed", 4 }
-					}
+					TotalStacks = new Dictionary<string, int>()
 				};
+			}
+			
+			var initialStackOptions = new Dictionary<string, int>
+			{
+				{"furnace", 3},
+				{"furnace.large", 15},
+				{"campfire", 2},
+				{"refinery_small_deployed", 4},
+				{"hobobarrel_static", 2}
+			};
+
+			var options = allPlayerOptions[player.userID];
+
+			foreach (var kv in initialStackOptions)
+			{
+				if (!options.TotalStacks.ContainsKey(kv.Key))
+					options.TotalStacks.Add(kv.Key, kv.Value);
 			}
 		}
 
